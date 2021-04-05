7e24d4f19a6fa78ed43789c4babf85aa9f379b91
PeHaa <info@pehaa.com>
2021-04-05 20:14:54 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import GradientsList from "./components/GradientsList"
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center my-4">Alyra Gradients</h1>
+      <main className="container">
+        <GradientsList />
+      </main>
+    </div>
+  )
+}
+
+export default App
