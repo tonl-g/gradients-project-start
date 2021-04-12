e817dd554c0db24b01ee14510760371c82c799b8
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-12 18:00:33 +0200

    with hooks

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import GradientHeader from './components/GradientHeader'
 import GradientApp from './components/GradientsApp'
 import Footer from './components/Footer'
 import { gradients, uniqueTags } from './gradients'
+import { useState } from 'react'
 
 
 function App() {
+const[grad , setGrad] = useState(gradients)
+const[tags , setTags] = useState(uniqueTags)
+
+
 
   return (
     <>
-      <GradientHeader gradients={gradients} />
-      <GradientApp gradients={gradients} uniqueTags={uniqueTags} />
+      <GradientHeader gradients={grad} />
+      <GradientApp gradients={grad} uniqueTags={tags} />
       < Footer />
     </>
   )
