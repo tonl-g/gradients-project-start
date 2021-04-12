fdf8266d0581405584e66aadd28fe0a7d43b3922
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-12 19:43:07 +0200

    yarn init

--- src/components/GradientsSelect.js
+++ src/components/GradientsSelect.js
@@ -3,12 +3,14 @@ const GradientSelect = () => {
 
   return (
     <>
-      <div className="input-group mb-3"><label className="input-group-text" htmlFor="select">Filtrer par tag</label><select
+      <div className="input-group mb-3"><label className="input-group-text" htmlFor="select">Filtrer par tag</label>
+      <select
         className="form-select" key="select">
         <option value="all">Tous</option>
         {uniqueTags.map(tag => <option key={tag} value={tag}>{tag}</option>)}
 
-      </select></div>
+      </select>
+      </div>
     </>
   )
 }
