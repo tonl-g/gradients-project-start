16f18110d832426ce95e0490a876dace43924ecb
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-12 20:01:43 +0200

    GradientsHeader

--- src/components/GradientHeader.js
+++ src/components/GradientHeader.js
@@ -4,6 +4,33 @@ const Header = () => {
       style={{"backgroundImage": "linear-gradient(to right, rgb(255, 0, 204), rgb(51, 51, 153))"}}>
         <h1 className="display-1">Alyra Gradients</h1>
         <p className="tagline">Ultime collection de plus beaux dégradés</p>
+        <button
+          aria-label="Clicker pour afficher le dégradé précédant" type="button" className="btn btn-outline-light m-1"><svg
+            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left"
+            viewBox="0 0 16 16">
+            <path fillRule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z">
+            </path>
+          </svg>
+        </button>
+        <button aria-label="Clicker pour changer le dégradé" type="button"
+          className="btn btn-outline-light m-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+            fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"></path>
+            <path
+              d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z">
+            </path>
+          </svg>
+        </button>
+        <button aria-label="Clicker pour afficher le dégradé suivant" type="button"
+          className="btn btn-outline-light m-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+            fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fillRule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z">
+            </path>
+          </svg>
+        </button>
+        
       </header>
   )
 }
