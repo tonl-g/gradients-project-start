6d2d80c5fe98c3fcb92ba505c99892f66d4b5880
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-12 19:28:16 +0200

    fix netlify

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
       <GradientHeader />
       <GradientApp />
-      < Footer />
+      <Footer />
     </>
   )
 }
--- src/components/GradientHeader.js
+++ src/components/GradientHeader.js
@@ -1,12 +1,10 @@
 const Header = () => {
   return (
-    <>
       <header className="text-center bg-dark text-white py-5 mb-5"
       style={{"backgroundImage": "linear-gradient(to right, rgb(255, 0, 204), rgb(51, 51, 153))"}}>
         <h1 className="display-1">Alyra Gradients</h1>
         <p className="tagline">Ultime collection de plus beaux dégradés</p>
       </header>
-    </>
   )
 }
 
