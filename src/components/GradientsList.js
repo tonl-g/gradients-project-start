6c558f955737852a9e2d40f3eabf1f232068e851
EmmanuelRatier <ratier.e09@gmail.com>
2021-04-12 17:42:09 +0200

    component gradient

--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -3,6 +3,16 @@ const GradientsList = (props) => {
   const { gradients, uniqueTags } = props
   return (
     <ul className="row list-unstyled">
+      {gradients.map(elem =>
+        <Gradient
+          key={elem.name}
+          colorStart={elem.start}
+          colorEnd={elem.end}
+          name={elem.name}
+        />
+      )}
+
+
       <Gradient
         colorStart="rgb(189, 195, 199)"
         colorEnd="rgb(44, 62, 80)"
