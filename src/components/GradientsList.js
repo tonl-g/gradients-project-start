3fc3c69f3fc988f05605dfc9030063154a11e434
tonl-g <tlygonneville.dev@gmail.com>
2021-04-14 09:20:45 +0200

    Modif

--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -2,7 +2,7 @@ import Gradient from "./Gradient"
 import {gradients} from '../gradients'
 
 const GradientsList = (props) => {
-  const {filter,setFilter} =props
+  const {filter,setFilter} = props
 
   let filteredList = ""
   filter === "All" ? filteredList = gradients : filteredList = gradients.filter(el => el.tags.includes(filter))
